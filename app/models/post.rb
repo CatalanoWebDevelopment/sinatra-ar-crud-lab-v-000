538725e7644e7a5a9f4fe7047c60d6e1f0ceb25e
Parker Catalano <catalano.work@gmail.com>
2018-02-02 20:11:29 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,4 @@
 class Post < ActiveRecord::Base
-  attr_accessor :name, :content
-  POSTS = []
 
-  def initialize(args)
-    @name = args[:name]
-    @content = args[:content]
-    POSTS << self
-  end
-
-  def self.all
-    POSTS
-  end
-
-  def self.clear
-    POSTS.clear
-  end
-
+  
 end
